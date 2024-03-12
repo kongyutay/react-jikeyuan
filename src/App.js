155c6612fe8a101e9ac827efb62f8a5496aa1d0f
Tay Kong Yu <kongyutay@gmail.com>
2024-03-12 22:28:10 +0800

    Init

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import {Button} from 'antd'
+
+function App(){
+  return <div>this is app</div>
+}
+
+export default App
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+//Router configuration
+import Layout from '@/pages/Layout';
+import Login from '@/pages/Login';
+
+import { createBrowserRouter } from 'react-router-dom'
+
+//配置路由实例
+const rounter = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <Layout />
+        },
+        {
+            path: "/login",
+            element: <Login />
+        }
+    ]
+)
+
+export default rounter
